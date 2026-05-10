0a46a7cb6df5c95f2ac41b04a6ab85570643cf7c
keigo <2364138keigo@gmail.com>
2026-05-10 16:38:30 +0900

    insert時のtripsの型を追加

--- src/types/trips.ts
+++ src/types/trips.ts
@@ -1,3 +1,4 @@
 import type { Database } from "../../schema";
 
 export type TripsType = Database["public"]["Tables"]["trips"]["Row"];
+export type InsertTripsType = Database["public"]["Tables"]["trips"]["Insert"];
