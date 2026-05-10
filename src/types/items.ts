e5c6166a2f7b059412e66ac2b741339eda6f3de5
keigo <2364138keigo@gmail.com>
2026-05-10 22:50:52 +0900

    ItemDetailTypeを定義し、アイテムの詳細情報を型として追加

--- src/types/items.ts
+++ src/types/items.ts
@@ -2,3 +2,11 @@ import type { Database } from "../../schema";
 
 export type ItemsType = Database["public"]["Tables"]["items"]["Row"];
 export type InsertItemsType = Database["public"]["Tables"]["items"]["Insert"];
+export type ItemDetailType = {
+  item_id: string;
+  is_checked: boolean;
+  items: {
+    id: string;
+    name: string;
+  };
+};
