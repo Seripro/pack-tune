bdf5afc37a66f98fcac585d9e1baa3b0f74f02e2
keigo <2364138keigo@gmail.com>
2026-05-10 20:34:57 +0900

    旅行の詳細画面へのリンクを追加

--- src/pages/Trips.tsx
+++ src/pages/Trips.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getTripsByUserId } from "../utils/supabaseFunctions";
 import type { TripsType } from "../types/trips";
 import { TripCard } from "../components/TripCard";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const user_id = "f1bdb7e9-102b-403a-9e7e-62801081d3a6";
 
@@ -26,15 +26,18 @@ export const Trips = () => {
       <p>Trips</p>
       {trips?.map((trip) => {
         return (
-          <TripCard
-            key={trip.id}
-            title={trip.title}
-            start_date={trip.start_date}
-            end_date={trip.end_date}
-            is_completed={trip.is_completed}
-          />
+          <Link to={`${trip.id}`}>
+            <TripCard
+              key={trip.id}
+              title={trip.title}
+              start_date={trip.start_date}
+              end_date={trip.end_date}
+              is_completed={trip.is_completed}
+            />
+          </Link>
         );
       })}
+
       <button onClick={() => navigate("/trips/new")}>新しい旅行</button>
     </>
   );
