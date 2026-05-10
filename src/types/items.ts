9469240a3cfe29be857dbb2b3782cd8a8b2af72e
keigo <2364138keigo@gmail.com>
2026-05-10 16:49:38 +0900

    insert時のitemsの型を追加

--- src/types/items.ts
+++ src/types/items.ts
@@ -1,3 +1,4 @@
 import type { Database } from "../../schema";
 
 export type ItemsType = Database["public"]["Tables"]["items"]["Row"];
+export type InsertItemsType = Database["public"]["Tables"]["items"]["Insert"];
