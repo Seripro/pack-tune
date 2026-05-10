266fcb03f471e19384b5780bd3952e047117689c
keigo <2364138keigo@gmail.com>
2026-05-10 09:35:34 +0900

    旅行一覧を表示

--- src/pages/Trips.tsx
+++ src/pages/Trips.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import { getAllTrips } from "../utils/supabaseFunctions";
+import type { TripsType } from "../types/trips";
+import { TripCard } from "../components/TripCard";
+import { useNavigate } from "react-router-dom";
+
 export const Trips = () => {
+  const [trips, setTrips] = useState<TripsType[]>();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const getTrips = async () => {
+      try {
+        const tripDatas = await getAllTrips();
+        setTrips(tripDatas);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getTrips();
+  }, []);
+
   return (
     <>
       <p>Trips</p>
+      {trips?.map((trip) => {
+        return (
+          <TripCard
+            key={trip.id}
+            title={trip.title}
+            start_date={trip.start_date}
+            end_date={trip.end_date}
+            is_completed={trip.is_completed}
+          />
+        );
+      })}
+      <button onClick={() => navigate("/trips/new")}>新しい旅行</button>
     </>
   );
 };
