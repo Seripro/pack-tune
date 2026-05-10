4a7ae74d77f27ba576150868c759335af3f96dac
keigo <2364138keigo@gmail.com>
2026-05-10 20:00:32 +0900

    trip_itemsの型を定義

--- src/types/trip_items.ts
+++ src/types/trip_items.ts
@@ -2,3 +2,5 @@ import type { Database } from "../../schema";
 
 export type InsertTripItemsType =
   Database["public"]["Tables"]["trip_items"]["Insert"];
+
+export type TripItemsType = Database["public"]["Tables"]["trip_items"]["Row"];
