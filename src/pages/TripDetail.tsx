b91a83a23a2dad8a5a91ea27434e54bd8ceb2dc5
keigo <2364138keigo@gmail.com>
2026-05-11 11:49:32 +0900

    旅行の詳細画面を作成

---
+++ src/pages/TripDetail.tsx
@@ -0,0 +1,125 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import {
+  deleteTripItem,
+  getItemsByTripId,
+  insertItems,
+  updateItems,
+} from "../utils/supabaseFunctions";
+import type { ItemDetailType } from "../types/items";
+
+const user_id = "f1bdb7e9-102b-403a-9e7e-62801081d3a6";
+
+export const TripDetail = () => {
+  const { tripId } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const title = location.state;
+
+  const [items, setItems] = useState<ItemDetailType[]>();
+  const [inputItem, setInputItem] = useState<string>("");
+
+  useEffect(() => {
+    const getItems = async () => {
+      if (tripId) {
+        const newItems = await getItemsByTripId(tripId);
+        setItems(newItems);
+      }
+    };
+    getItems();
+  }, []);
+
+  const handleCheck = async (
+    item_id: string,
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const updateCheck = async () => {
+      try {
+        await updateItems(e.target.checked, item_id, tripId!);
+        const newItems = [...(items || [])].map((item) => {
+          if (item.item_id == item_id) {
+            return { ...item, is_checked: !item.is_checked };
+          } else {
+            return item;
+          }
+        });
+        setItems(newItems);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    await updateCheck();
+  };
+
+  const onClickDelete = async (item_id: string) => {
+    const deleteItem = async () => {
+      try {
+        await deleteTripItem(item_id, tripId!);
+        const newItems = [...(items || [])].filter(
+          (item) => item.item_id !== item_id,
+        );
+        setItems(newItems);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    await deleteItem();
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputItem(e.target.value);
+  };
+
+  const onClickAdd = async () => {
+    const addItems = async () => {
+      try {
+        const data = await insertItems([{ user_id: user_id, name: inputItem }]);
+        setItems([
+          ...(items || []),
+          {
+            item_id: data[0].id,
+            is_checked: false,
+            items: {
+              id: data[0].id,
+              name: data[0].name,
+            },
+          },
+        ]);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    await addItems();
+    setInputItem("");
+  };
+
+  return (
+    <>
+      <p>{title}</p>
+      <div>
+        <input
+          placeholder="持ち物を追加"
+          value={inputItem}
+          onChange={handleChange}
+        />
+        <button onClick={onClickAdd}>＋</button>
+      </div>
+      {items?.map((item) => {
+        return (
+          <div key={item.item_id}>
+            <input
+              type="checkbox"
+              checked={item.is_checked}
+              onChange={(e) => handleCheck(item.item_id, e)}
+            />
+            <p>{item.items.name}</p>
+            <button onClick={() => onClickDelete(item.item_id)}>削除</button>
+          </div>
+        );
+      })}
+      <button onClick={() => navigate(`/trips/${tripId}/feedback`)}>
+        旅行を終了する
+      </button>
+    </>
+  );
+};
