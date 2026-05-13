e585b07ee6cd0ac994cfaddb544382eaf1e21942
keigo <2364138keigo@gmail.com>
2026-05-13 22:51:24 +0900

    TypeError: window.matchMedia is not a function を解消

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,10 +1,25 @@
 import { cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import '@testing-library/jest-dom';
-import { afterEach } from 'vitest'
+import "@testing-library/jest-dom";
+import { afterEach } from "vitest";
+import { vi } from "vitest";
 
 export const user = userEvent.setup();
 
 afterEach(() => {
   cleanup();
+});
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
 });
